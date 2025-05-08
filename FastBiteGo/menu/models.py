b7b4387c51f3b794274094e1e87e4ad58683ef3d
Slekatom <nemo.bma@gmail.com>
2025-05-08 09:24:57 +0300

    Models register

--- FastBiteGo/menu/models.py
+++ FastBiteGo/menu/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=50)
+    media = models.ImageField(upload_to='media/category')
+
+    def __str__(self):
+        return f"Category {self.title}"
+
+class Meal(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=50)
+    media = models.ImageField(upload_to='media/category')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} in category {self.category}"
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.TextField(max_length=30)
+    rating = models.FloatField(
+        validators=[MinValueValidator(0), MaxValueValidator(5)]
+    )
+
+    def __str__(self):
+        return f"{self.user} reviewed on {self.meal}: {self.title} -> {self.rating}"
