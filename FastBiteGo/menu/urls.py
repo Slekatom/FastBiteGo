04719e5e8aee27e8ed16847ab1f278943050eb39
Slekatom <nemo.bma@gmail.com>
2025-05-18 18:53:05 +0300

    detail view for categories

--- FastBiteGo/menu/urls.py
+++ FastBiteGo/menu/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.MenuList.as_view(), name = "landing"),
+    path("<int:pk>/", views.CategoryDetailView.as_view(), name = "meals"),
 ]
--- FastBiteGo/menu/views.py
+++ FastBiteGo/menu/views.py
@@ -12,3 +12,14 @@ class MenuList(ListView):
         context = super().get_context_data(**kwargs)
         context["categories"] = Category.objects.all()
         return context
+
+class CategoryDetailView(DetailView):
+    model = Category
+    template_name = "menu/meal_list.html"
+    context_object_name = "data"
+
+    def get_context_data(self, **kwargs):
+        category = self.get_object()
+        context = super().get_context_data(**kwargs)
+        context["category"] = category
+        return context
