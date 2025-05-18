6aa1b8c84a67d4ef5a358dd2278d3c06169af1ec
Slekatom <nemo.bma@gmail.com>
2025-05-18 19:20:16 +0300

    custom user views

--- FastBiteGo/accounts/views.py
+++ FastBiteGo/accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from .forms import *
+from django.views.generic import CreateView
+from django.urls import reverse
 
-# Create your views here.
+class LoginView(CreateView):
+    model = CustomUser
+    template_name = "accounts/login.html"
+    from_class = AuthenticationForm
+
+    def get_success_url(self):
+        return redirect(reverse('landing:'))
+
+class RegisterView(CreateView):
+    model = CustomUser
+    template_name = "accounts/register.html"
+    from_class = CustomUserCreationForm
+
+    def get_success_url(self):
+        return redirect(reverse("landing:"))
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
