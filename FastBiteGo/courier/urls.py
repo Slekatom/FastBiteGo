75c33a6dfe0e7c911b11a410684be4c7b3261135
Slekatom <nemo.bma@gmail.com>
2025-06-21 23:54:37 +0300

    request update

--- FastBiteGo/courier/urls.py
+++ FastBiteGo/courier/urls.py
@@ -4,5 +4,6 @@ from .views import *
 app_name = "courier"
 
 urlpatterns = [
-    path("requests/", RequestList.as_view(), name = "requests")
+    path("requests/", RequestList.as_view(), name = "requests"),
+    path("requests/<int:pk>", RequestUpdate.as_view(), name = "update")
 ]
--- FastBiteGo/courier/views.py
+++ FastBiteGo/courier/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, UpdateView
 from .models import *
 
 class RequestList(ListView):
     model = Request
     template_name = "courier/requests.html"
     context_object_name = "requests"
+
+class RequestUpdate(UpdateView):
+    model = Request
+    template_name = "courier/take_request.html"
+    success_url = reverse_lazy("courier:requests")
+    fields = []
+    context_object_name = "request"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.status = "Is Taken"
+        form.save()
+        return super().form_valid(form)
