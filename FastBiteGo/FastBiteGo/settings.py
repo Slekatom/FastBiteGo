9e3294b7a97bf508a2de45b0442842aaf41ba26f
Slekatom <nemo.bma@gmail.com>
2025-06-22 19:15:52 +0300

    added .env

--- FastBiteGo/FastBiteGo/settings.py
+++ FastBiteGo/FastBiteGo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l1e@egm*@6*g#=fc02ep36a+6_$wh(ba&ing^bd6@%x6vlq4q6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == True
 
 ALLOWED_HOSTS = []
 
