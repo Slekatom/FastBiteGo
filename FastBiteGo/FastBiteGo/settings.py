db64df401d78ccf1413b147f7eded22c59d5a55f
Slekatom <nemo.bma@gmail.com>
2025-06-23 13:28:25 +0300

    changes

--- FastBiteGo/FastBiteGo/settings.py
+++ FastBiteGo/FastBiteGo/settings.py
@@ -26,9 +26,10 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") == True
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'yourdomain.com']
+
 
 
 # Application definition
