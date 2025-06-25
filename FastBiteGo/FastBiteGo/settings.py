ae9638bcadb710c5ccd847023f44e2bcede16954
Slekatom <nemo.bma@gmail.com>
2025-06-25 18:06:09 +0300

    prodaction version

--- FastBiteGo/FastBiteGo/settings.py
+++ FastBiteGo/FastBiteGo/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l1e@egm*@6*g#=fc02ep36a+6_$wh(ba&ing^bd6@%x6vlq4q6'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-dev-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yourusername.pythonanywhere.com', 'bunkeronline.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -125,7 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.getenv("DJANGO_STATIC_ROOT", os.path.join(BASE_DIR, 'staticfiles'))
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- FastBiteGo/FastBiteGo/wsgi.py
+++ FastBiteGo/FastBiteGo/wsgi.py
@@ -1,16 +1,29 @@
-"""
-WSGI config for FastBiteGo project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
+# This file contains the WSGI configuration required to serve up your
+# web application at http://bunkeronline.pythonanywhere.com/
+# It works by setting the variable 'application' to a WSGI handler of some
+# description.
+#
+# The below has been auto-generated for your Django project
 
 import os
+import sys
+from dotenv import load_dotenv
 
+# Путь к вашему файлу .env
+dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+
+# Загрузка переменных окружения из файла .env
+load_dotenv(dotenv_path)
+
+# add your project directory to the sys.path
+project_home = '/home/bunkeronline/django-bunker-online'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# set environment variable to tell django where your settings.py is
+os.environ['DJANGO_SETTINGS_MODULE'] = 'FastBiteGo.settings'
+
+
+# serve django via WSGI
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FastBiteGo.settings')
-
 application = get_wsgi_application()
