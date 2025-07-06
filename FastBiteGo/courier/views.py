8e39600a800346511f5b3025a9f181522098b250
Slekatom <nemo.bma@gmail.com>
2025-07-06 14:52:51 +0300

    chat

--- FastBiteGo/courier/views.py
+++ FastBiteGo/courier/views.py
@@ -89,7 +89,7 @@ class MessageCreate(LoginRequiredMixin, CreateView):
         return super().form_invalid(form)
 
     def get_success_url(self):
-        return reverse_lazy("courier:chat", kwargs = {"chat_pk": self.kwargs["chat_pk"]})
+        return reverse_lazy("courier:chat", kwargs={"chat_pk": self.object.chat.id})
 
     def get_context_data(self, **kwargs):
         chat_id = self.kwargs.get("chat_pk")
