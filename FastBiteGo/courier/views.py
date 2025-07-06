c91366c2961717219c2da087811aaa176f08765f
Slekatom <nemo.bma@gmail.com>
2025-07-06 18:16:10 +0300

    issue

--- FastBiteGo/courier/views.py
+++ FastBiteGo/courier/views.py
@@ -102,4 +102,5 @@ class MessageCreate(LoginRequiredMixin, CreateView):
         context = super().get_context_data(**kwargs)
         context["messages"] = Message.objects.filter(chat = chat)
         context["user_now"] = self.request.user
+        context["request_ob"] = self.get_object()
         return context
