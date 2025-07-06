bd2003c09c805d2dcca8d4f6a8c90d910a579da2
Slekatom <nemo.bma@gmail.com>
2025-07-06 12:54:41 +0300

    registration form beauty

--- FastBiteGo/accounts/forms.py
+++ FastBiteGo/accounts/forms.py
@@ -8,7 +8,22 @@ from crispy_forms.layout import Submit
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ["username", "first_name", "last_name", "address"]
+        fields = ["username", "first_name", "last_name", "address", "password1", "password2"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Зареєструватися'))
+
+        # Optional: кастомізація label'ів
+        self.fields['username'].label = "Ім'я користувача"
+        self.fields['first_name'].label = "Ім'я"
+        self.fields['last_name'].label = "Прізвище"
+        self.fields['address'].label = "Адреса"
+        self.fields['password1'].label = "Пароль"
+        self.fields['password2'].label = "Підтвердження пароля"
 
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.CharField(
