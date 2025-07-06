2844e68ebbc2db854aa85fc4a05f649c5a739f9d
Slekatom <nemo.bma@gmail.com>
2025-07-06 15:07:59 +0300

    v4

--- FastBiteGo/courier/views.py
+++ FastBiteGo/courier/views.py
@@ -48,7 +48,7 @@ class RequestUpdate(LoginRequiredMixin, UpdateView):
         if form.instance.courier != form.instance.user:
             form.save()
             request = form.instance
-            chat = Chat.objects.get(request=request)
+            chat = Chat.objects.get(request = request)
             url = reverse('courier:detail', args=[chat.request.id])
             Message.objects.create(user = chat.courier,
                                    text = f"Кур'єр {chat.courier} прийняв замовлення {chat.user}. Переглянути замовлення: {url}",
@@ -89,7 +89,7 @@ class MessageCreate(LoginRequiredMixin, CreateView):
         return super().form_invalid(form)
 
     def get_success_url(self):
-        return reverse_lazy("courier:chat", kwargs={"chat_pk": self.object.chat.id})
+        return reverse_lazy("courier:chat", kwargs = {"chat_pk": self.kwargs["chat_pk"]})
 
     def get_context_data(self, **kwargs):
         chat_id = self.kwargs.get("chat_pk")
