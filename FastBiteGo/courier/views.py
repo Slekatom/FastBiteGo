26d293e6665b62e23911cfe836b2d9478ea97cd5
Slekatom <nemo.bma@gmail.com>
2025-07-06 14:49:53 +0300

    chat issue

--- FastBiteGo/courier/views.py
+++ FastBiteGo/courier/views.py
@@ -48,6 +48,7 @@ class RequestUpdate(LoginRequiredMixin, UpdateView):
         if form.instance.courier != form.instance.user:
             form.save()
             request = form.instance
+            chat = Chat.objects.get(request=request)
             url = reverse('courier:detail', args=[chat.request.id])
             Message.objects.create(user = chat.courier,
                                    text = f"Кур'єр {chat.courier} прийняв замовлення {chat.user}. Переглянути замовлення: {url}",
