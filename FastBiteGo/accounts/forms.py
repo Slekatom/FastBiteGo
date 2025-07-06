642f83fe25b7088d44bc654e911c483f833ae81e
Slekatom <nemo.bma@gmail.com>
2025-07-06 12:51:48 +0300

    login form beauty

--- FastBiteGo/accounts/forms.py
+++ FastBiteGo/accounts/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from .models import *
+from django.utils.translation import gettext_lazy as _
+from .models import CustomUser
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
@@ -8,9 +11,21 @@ class CustomUserCreationForm(UserCreationForm):
         fields = ["username", "first_name", "last_name", "address"]
 
 class CustomAuthenticationForm(AuthenticationForm):
-    class Meta:
-        model = CustomUser
-        fields = ["first_name", "password"]
+    username = forms.CharField(
+        label=_("Ім'я користувача"),
+        widget=forms.TextInput(attrs={"autofocus": True})
+    )
+    password = forms.CharField(
+        label=_("Пароль"),
+        strip=False,
+        widget=forms.PasswordInput
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Увійти'))
 
 
 
