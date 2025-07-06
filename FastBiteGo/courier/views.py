beb16434aa087d7edef42357b6650f915c9a8c4b
Slekatom <nemo.bma@gmail.com>
2025-07-06 16:23:29 +0300

    solved

--- FastBiteGo/courier/views.py
+++ FastBiteGo/courier/views.py
@@ -48,9 +48,9 @@ class RequestUpdate(LoginRequiredMixin, UpdateView):
         if form.instance.courier != form.instance.user:
             form.save()
             request = form.instance
-            chat = Chat.objects.create(user = request.user,
-                                courier = self.request.user,
-                                request = request)
+            chat = Chat.objects.get(request = request)
+            chat.courier = self.request.user
+            chat.save()
             url = reverse('courier:detail', args=[chat.request.id])
             Message.objects.create(user = chat.courier,
                                    text = f"Кур'єр {chat.courier} прийняв замовлення {chat.user}. Переглянути замовлення: {url}",
